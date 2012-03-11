e8cbfcb31ef3319c39c520cd662a2d07edc68c47
Augusto <Augusto.Rodriguez@gmail.com>
2012-03-11 22:24:38 +0000

    Updated project to work properly with maven 3 and removed unicode character from Paperboy.java

--- src/main/java/org/workshop/code/law_of_demeter/correct/Paperboy.java
+++ src/main/java/org/workshop/code/law_of_demeter/correct/Paperboy.java
@@ -3,7 +3,7 @@ package org.workshop.code.law_of_demeter.correct;
 public class Paperboy {
 
 	public void billCustomer(Customer customer) {
-		float payment = 2; // �I want my two dollars!�
+		float payment = 2; // "I want my two dollars!"
 		boolean paymentTaken = customer.getPayment(payment);
 		if (paymentTaken) {
 			// say thank you and give customer a receipt
